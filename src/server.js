01b65cd05db84a5f19ecee8f71992bd1e19f1d9a
jerry Perez <jerryperezxbox@gmail.com>
2023-10-29 20:42:45 -0600

    server.js moved

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 const app = express();
 
 app.use(express.static(__dirname+'/dist/ensolvers'));
-app.get('/*',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/ensolvers/index.html'));
+
+app.all('*', (req, res) => {
+  res.status(200).sendFile(__dirname + '/dist/ensolvers/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
